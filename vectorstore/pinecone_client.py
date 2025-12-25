0a2d448837d6ee1ff1fd77da11215c329ee4d1ea
Abhiram Mulinti <abhirammulinti@Abhirams-MacBook-Air.local>
2025-12-24 23:09:16 -0700

    Added the query_vector function

--- vectorstore/pinecone_client.py
+++ vectorstore/pinecone_client.py
@@ -22,3 +22,17 @@ def upsert_chunk(chunk_id: str, text: str, metadata: dict):
     index.upsert(
         vectors=[{"id": chunk_id, "values": vector_values, "metadata": metadata}]
     )
+
+
+def query_vector(query_text: str, top_k: int = 3):
+    """Search for the most similar chunks and return them"""
+    embeddings = pc.inference.embed(
+        model="llama-text-embed-v2",
+        inputs=[query_text],
+        parameters={"input_type": "query"},
+    )
+
+    results = index.query(
+        vector=embeddings.data[0].values, top_k=top_k, include_metadata=True
+    )
+    return results
